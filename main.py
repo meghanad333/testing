a356052d3ea45e4f38ba08495ecebb3f623861d5
meghanad333 <meghanad.kheti@cur8.in>
2025-04-03 11:09:01 +0530

    adding version

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from addition import addition
+from test import testing_with_ai
+
 print("hello world")
+print(addition(5,6))
+
+function_string = """
+def addition(a, b):
+    return a + b
+    
+"""
+
+print(testing_with_ai(function_string))
